836b6a6ddc81a0cfeeac7fb9c123b4d336004309
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2023-01-08 11:49:01 -0500

    Refactoring of component App - done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,25 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
 import { Searchbar } from "components/Searchbar/Searchbar";
 import { ImageGallery } from "components/ImageGallery/ImageGallery"
 
 import { Container } from './App.styled'
 
-export class App extends Component {
-  state = {
-    searchingImageName: '',
+export function App() {
+  const [searchingImageName, setSearchingImageName] = useState('')
+
+  const handleFormSubmit = searchingImageName => {
+    setSearchingImageName(searchingImageName)
   }
 
-  handleFormSubmit = searchingImageName => {
-    this.setState({ searchingImageName })
-  }
-
-  render() {
-    const { searchingImageName } = this.state
-    return (
-      <Container>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery searchingImageName={searchingImageName} />
-        <ToastContainer position="top-center" autoClose={2000} />
-      </Container >
-    );
-  }
-};
+  return (
+    <Container>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery searchingImageName={searchingImageName} />
+      <ToastContainer position="top-center" autoClose={2000} />
+    </Container >
+  );
+}
 
 
