a457f93292bda06f66786261e37ada39598f6acf
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2023-01-08 19:55:47 -0500

    Refactoring of component ImageGallery - done

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types'
 
@@ -11,61 +11,53 @@ import { ImageGalleryList } from "./ImageGallery.styled"
 
 const API_KEY = '31359912-5b88546d239f41508b7e3830d'
 
-export class ImageGallery extends Component {
-    state = {
-        images: [],
-        loading: false,
-        page: 1,
-    }
+export function ImageGallery({ searchingImageName }) {
+    const [images, setImages] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [page, setPage] = useState(1)
+    const [name, setName] = useState('')
 
-    componentDidUpdate(prevProps, prevState) {
-        const prevName = prevProps.searchingImageName
-        const newName = this.props.searchingImageName
-        const { page, images } = this.state
+    useEffect(() => {
+        setImages([])
+        setName(searchingImageName)
+    }, [searchingImageName])
 
-        if (prevName !== newName) {
-            this.setState({ images: [], page: 1 })
-        }
-
-        if (prevName !== newName || prevState.page !== page) {
-            this.setState({ loading: true });
-            fetch(`https://pixabay.com/api/?q=${newName}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+    useEffect(() => {
+        if (name !== '') {
+            setLoading(true)
+            fetch(`https://pixabay.com/api/?q=${name}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
                 .then(responce => {
                     if (responce.ok) {
                         return responce.json()
                     }
                     return Promise.reject(
-                        new Error(toast.error(`We don't have images ${newName}`))
+                        new Error(toast.error(`We don't have images ${name}`))
                     )
                 })
-                .then(image => this.setState((prevState) => ({ images: [...images, ...image.hits] })))
-                .catch(() => toast.error(`We don't have images ${newName}`))
-                .finally(() => this.setState({ loading: false }))
+                .then(image => setImages(prevState => [...prevState, ...image.hits]))
+                .catch(() => toast.error(`We don't have images ${name}`))
+                .finally(() => setLoading(false))
         }
+    }, [name, page])
+
+    const onClickLoadMore = () => {
+        setPage(prevState => prevState + 1)
     }
 
-    onClickLoadMore = () => {
-        this.setState(prevState => ({ page: prevState.page + 1 }))
-    }
+    return (
+        <div>
+            {loading && <Loader />}
+            <ImageGalleryList>
 
-    render() {
-        const { images, loading } = this.state
-        return (
-            <div>
-                {loading && <Loader />}
-                <ImageGalleryList>
+                {images.map(({ id, webformatURL, largeImageURL }) => (
+                    <ImageGalleryItem key={id} webformatURL={webformatURL} largeImageURL={largeImageURL} />
+                ))}
+            </ImageGalleryList >
+            {images.length !== 0 && <ButtonLoadMore onClickLoadMore={onClickLoadMore} />}
+        </div>
 
-                    {images.map(({ id, webformatURL, largeImageURL }) => (
-                        <ImageGalleryItem key={id} webformatURL={webformatURL} largeImageURL={largeImageURL} />
-                    ))}
-                </ImageGalleryList >
-                {images.length !== 0 && <ButtonLoadMore onClickLoadMore={this.onClickLoadMore} />}
-            </div>
-
-        )
-    }
+    )
 }
-
 
 ImageGallery.propTypes = {
     searchingImageName: PropTypes.string.isRequired,
